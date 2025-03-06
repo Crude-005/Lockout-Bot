35d79968432ef55c0695f7ba7f18e9b87414c518
“Adya <adya.singhal@gmail.com>
2025-03-06 18:13:47 +0530

    Added Duels database

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from leaderboard import send_leaderboard
 from userVerification import userRegistration
 from dotenv import load_dotenv 
+from duelsData import create_duel, accept_duel, get_pending_duel
 load_dotenv() 
 
 
@@ -28,6 +29,38 @@ async def on_message(message):
         return
 
     elif message.content.startswith("$duel"):
+        parts = message.content.split()
+        if len(parts) != 2 or not message.mentions:
+            await message.channel.send("Usage: `$duel @opponent`")
+            return
+        
+        opponent = message.mentions[0]
+        duel_id = create_duel(message.guild.id, message.author.id, opponent.id)
+        
+        await message.channel.send(
+            f"{opponent.mention}, you have been challenged by {message.author.mention}! "
+            f"Type `$acceptduel @{message.author}` to accept."
+        )
+        return
+    
+    elif message.content.startswith("$acceptduel"):
+        parts = message.content.split()
+        if len(parts) != 2 or not message.mentions:
+            await message.channel.send("Usage: `$acceptduel @challenger`")
+            return
+        
+        challenger = message.mentions[0]
+        duel = get_pending_duel(message.guild.id, challenger.id, message.author.id)
+
+        if not duel:
+            await message.channel.send("No pending duel found!")
+            return
+        
+        accept_duel(message.guild.id, challenger.id, message.author.id)
+        
+        await message.channel.send(
+            f"Duel accepted between {challenger.mention} and {message.author.mention}! Good luck!"
+        )
         return
         
 
